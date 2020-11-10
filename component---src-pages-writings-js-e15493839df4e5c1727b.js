917e1c970d01202bad070822a18d17ee1aa9f9e0
Niklas Buhl <hello@niklasbuhl.com>
2020-11-10 18:06:52 +0100

    Updates

---
+++ component---src-pages-writings-js-e15493839df4e5c1727b.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[21],{uJdc:function(n,t,e){"use strict";e.r(t);var r=e("dI71"),a=e("q1tI"),o=e.n(a),u=(e("nDTf"),e("BEva")),c=e("BJ7y"),i=(e("7p3S"),function(n){function t(){return n.apply(this,arguments)||this}Object(r.a)(t,n);var e=t.prototype;return e.didComponentMount=function(){},e.render=function(){return o.a.createElement(u.a,null,o.a.createElement(c.a,null,o.a.createElement("h1",null,"Writings")))},t}(a.Component));t.default=i}}]);
+//# sourceMappingURL=component---src-pages-writings-js-e15493839df4e5c1727b.js.map
